819535de9822c612c91fa5f688e02180709f8b2e
Tejodeep Bose <tejodeepbose99@gmail.com>
2025-08-22 14:12:19 +0530

    initialize Spring Boot application with basic structure and configurations

--- src/main/java/com/tejo/appdemo/controller/TejoController.java
+++ src/main/java/com/tejo/appdemo/controller/TejoController.java
@@ -1,4 +1,3 @@
-
 package com.tejo.appdemo.controller;
 
 import com.tejo.appdemo.entity.TejoEntity;
@@ -7,17 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/tejo")
 public class TejoController {
 
+    private final TejoService tejoService;
+
     @Autowired
-    private TejoService tejoService;
+    public TejoController(TejoService tejoService) {
+        this.tejoService = tejoService;
+    }
 
     // CREATE
     @PostMapping
     public ResponseEntity<TejoEntity> createTejo(@RequestBody TejoEntity tejo) {
+        log.info("Creating a new Tejo entity: {}", tejo);
         TejoEntity saved = tejoService.saveTejo(tejo);
         return ResponseEntity.ok(saved);
     }
@@ -25,6 +31,7 @@ public class TejoController {
     // READ ALL
     @GetMapping
     public ResponseEntity<List<TejoEntity>> getAllTejos() {
+        log.info("Fetching all Tejo entities");
         List<TejoEntity> list = tejoService.getAllTejos();
         return ResponseEntity.ok(list);
     }
@@ -32,6 +39,7 @@ public class TejoController {
     // READ BY ID
     @GetMapping("/{id}")
     public ResponseEntity<TejoEntity> getTejoById(@PathVariable Integer id) {
+        log.info("Fetching Tejo entity with ID: {}", id);
         return tejoService.getTejoById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
@@ -40,7 +48,8 @@ public class TejoController {
     // UPDATE BY ID
     @PutMapping("/{id}")
     public ResponseEntity<TejoEntity> updateTejo(@PathVariable Integer id, @RequestBody TejoEntity tejo) {
-        if (!tejoService.getTejoById(id).isPresent()) {
+        log.info("Updating Tejo entity with ID: {}", id);
+        if (tejoService.getTejoById(id).isEmpty()) {
             return ResponseEntity.notFound().build();
         }
         TejoEntity updated = tejoService.updateTejo(id, tejo);
@@ -50,7 +59,7 @@ public class TejoController {
     // DELETE BY ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTejo(@PathVariable Integer id) {
-        if (!tejoService.getTejoById(id).isPresent()) {
+        if (tejoService.getTejoById(id).isEmpty()) { // Updated from isPresent() to isEmpty()
             return ResponseEntity.notFound().build();
         }
         tejoService.deleteTejo(id);
